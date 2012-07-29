908da72ea8c2c64b1c34f3db5d72f1ccaa1cc7e9
Joseph D. Olivarez <jdolivarez@gmail.com>
2012-07-28 23:33:36 -0500

    before add column

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,10 @@ class Item < ActiveRecord::Base
 	belongs_to :user
 	has_one :location
 	has_one :searchstatus
+	after_initialize :init
+	def init
+      self.numofsearch  ||= 0          #will set the default value only if it's nil
+    end
+
   attr_accessible :author, :callnumber, :comments, :numofsearch, :title, :volume
 end
