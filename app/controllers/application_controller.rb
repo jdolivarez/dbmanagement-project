9800d9c016837bec3d9b18aef1b4f472830960d6
Joseph D. Olivarez <jdolivarez@gmail.com>
2012-08-06 20:16:42 -0500

    initial deploy commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def after_sign_in_path_for(employee)
-    items_path
+ def after_sign_in_path_for(employee)
+ items_path
  end
- def after_sign_out_path_for(employee)
- pages_path
- end
+def after_sign_out_path_for(employee)
+pages_path
+end
  def after_sign_out_path_for(user)
  pages_path
  end
